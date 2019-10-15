e45d9f7530d39eefd1e90e71179e2819e0c963c2
VladShevchenko <vlad-shev@ukr.net>
2019-10-15 03:43:51 +0300

    Initial commit for usermanagement

---
+++ usermanagement/src/main/java/ua/nure/kn/shevchenko/usermanagement/User.java
@@ -0,0 +1,58 @@
+package ua.nure.kn.shevchenko.usermanagement;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class User implements Serializable {
+	static final long serialVersionUID = -6321546794596617232L;
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    
+    public User(long id, String firstName, String lastName, Date dateOfBirth) {
+    	this.id = id;
+    	this.firstName = firstName;
+    	this.lastName = lastName;
+    	this.dateOfBirth = dateOfBirth;
+    }
+    
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getFullName(){
+		return lastName + " " + firstName; 
+	}
+	
+	public int getAge() {
+		LocalDate today = LocalDate.now();
+		LocalDate birthDate = dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(); 
+		Period p = Period.between(birthDate, today);
+		return p.getYears();
+    }
+
+}
---
+++ usermanagement/src/test/java/ua/nure/kn/shevchenko/usermanagement/UserTest.java
@@ -0,0 +1,42 @@
+package ua.nure.kn.shevchenko.usermanagement;
+
+import junit.framework.TestCase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class UserTest extends TestCase {
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final String FIRST_NAME = "Vladyslav";
+    private static final String LAST_NAME = "Shevchenko";
+    private static final String BIRTH_DATE = "2000-10-15";
+    private static final int AGE = 19;
+    
+    private User user;
+    
+    protected void setUp() throws Exception{
+        super.setUp();
+        this.user = new User(1L, FIRST_NAME, LAST_NAME, DATE_FORMAT.parse(BIRTH_DATE));
+    }
+
+    public void testGetFullName() {
+        assertEquals("Shevchenko Vladyslav", user.getFullName());
+    }
+    
+    public void testGetAge(){
+        assertEquals(AGE, user.getAge());
+    }
+    
+    public void testGetAgeToday(){
+    	User test_user = new User(1L, FIRST_NAME, LAST_NAME, new Date());
+        assertEquals(0, test_user.getAge());
+    }
+    public void testGetAgeTodayYearsAgo(){
+    	LocalDate localdate = LocalDate.now().minusDays(365);
+    	Date date = Date.from(localdate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    	User test_user = new User(1L, FIRST_NAME, LAST_NAME, date);
+        assertEquals(AGE-1, test_user.getAge());
+        }
+  }
